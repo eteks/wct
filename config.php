7e06e294978aed8f9cd2b5618c86e34e3478f27c
spmuthu21 <muthu@globalensolutions.com>
2016-02-05 19:31:05 +0530

    change in config.php

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
-define("DB_SERVER","localhost");
-define("DB_SERVER_USERNAME",'root');
-define("DB_SERVER_PASSWORD",'');
+define("DB_HOST","localhost");
+define("DB_USER",'root');
+define("DB_PASSWORD",'');
 define("DB_DATABASE",'welocity');
-$con =  mysql_connect(DB_SERVER,DB_SERVER_USERNAME,DB_SERVER_PASSWORD)or die("fail to connect".mysql_error());
-mysql_select_db(DB_DATABASE,$con) or die(mysql_error("Database is not Connected"));
 ?>
