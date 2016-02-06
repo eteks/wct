2735a8989b5394e78e0c3439067f8bba01653800
saisasthaa <saisasthaa@gmail.com>
2016-02-06 19:16:15 +0530

    update conflicts fixed code

--- footer.php
+++ footer.php
@@ -1,9 +1,4 @@
-
-<<<<<<< HEAD
-  <div class="footer col-md-12">
-=======
-<div class="footer">
->>>>>>> 5916c0426c19b6a3a4f803ffb2b87f65223e6acb
+<div class="footer col-md-12">
     <span class="footer_txt col-md-12">@2016 Atomicka.All Right Reserved.</span>
 </div>
 </body>
--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 			// Login Failed
 			echo "<script>alert('Emailid / Password Not Match')</script>";
 		}
+	}
 ?>
 <form role="form" action="" method="post" name="login_form">
 	<div class="select_user_content">
