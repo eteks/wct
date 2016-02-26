0047271caaf6a0041e7da903fc13dd0a25463ae1
spmuthu21 <muthu@globalensolutions.com>
2016-02-26 13:24:48 +0530

    change in date format

--- create_schedule.php
+++ create_schedule.php
@@ -1,20 +1,20 @@
 <?php require_once "session.php";
-	  require_once "header.php"; 
+	  require_once "header.php";
 	  require_once "functions/create_schedule_function.php";
 	  require_once 'functions/test_battery_functions.php';
-	  $createscheduleFunction = new createscheduleFunction();	  
-	  $testbattery = new testbatteryfunction();  
+	  $createscheduleFunction = new createscheduleFunction();
+	  $testbattery = new testbatteryfunction();
 ?>
 <div class="container">
 	<div class="container align_center align_height">
 		<span class="sports">CREATE SCHEDULE</span>
-	</div><!--end container-->	
+	</div><!--end container-->
 		<div class="col-xs-12 col-md-11">
 			<div class="col-md-4 hidden-xs"></div>
 			<div class="col-xs-12 col-md-7 align_margin">
 				<form name="create_schedule_form" id="createschedule_form">
 				<input type="hidden" class="statesid" name="edit_schedule_id">
-					<div class="form-group">					
+					<div class="form-group">
 						<label>Schedule Name</label><br>
 						<input type="text" class="adjust_width" name="schedule_name" data-validation-error-msg="Please Enter the name of the Schedule" data-validation="required">
 					</div>
@@ -26,14 +26,14 @@
 	                        $query = $testbattery->testbatterySelect();
 	                        while ($row = mysql_fetch_array($query)) {
 	                            ?>
-	                            <option value="<?php echo $row['testbattery_id']; ?>"><?php echo $row['testbattery_name']; ?></option>	                                            
-	                      <?php } ?>						   
+	                            <option value="<?php echo $row['testbattery_id']; ?>"><?php echo $row['testbattery_name']; ?></option>
+	                      <?php } ?>
 					  </select>
-					</div>			
+					</div>
 					<div class="form-group">
 					  <label for="date" class="fl">Select date</label><br>
-				    	<input class="date_pick" type="text">					  
-					</div>				
+				    	<input class="date_pick" type="text">
+					</div>
 					<div class="form-group col-md-12">
 					  <label for="date" class="fl" style="position: relative; left: -10px;">Select Time</label><br>
 					  	<div class="form-group align-time col-md-3">
@@ -42,7 +42,7 @@
 							    <option value="01">01</option>
 							    <option value="02">02</option>
 							    <option value="03">03</option>
-							    <option value="04">04</option>						   
+							    <option value="04">04</option>
 							    <option value="05">05</option>
 							    <option value="06">06</option>
 							    <option value="07">07</option>
@@ -54,7 +54,7 @@
 							    <option value="01">13</option>
 							    <option value="02">14</option>
 							    <option value="03">15</option>
-							    <option value="04">16</option>						   
+							    <option value="04">16</option>
 							    <option value="05">17</option>
 							    <option value="06">18</option>
 							    <option value="07">19</option>
@@ -62,7 +62,7 @@
 							    <option value="09">21</option>
 							    <option value="10">22</option>
 							    <option value="11">23</option>
-							    <option value="12">24</option>						   
+							    <option value="12">24</option>
 							</select>
 						</div>
 						<div class="form-group align-time col-md-3">
@@ -88,7 +88,7 @@
 							    <option value="17">17</option>
 							    <option value="18">18</option>
 							    <option value="19">19</option>
-							    <option value="20">20</option>				   
+							    <option value="20">20</option>
 							    <option value="21">21</option>
 							    <option value="22">22</option>
 							    <option value="23">23</option>
@@ -127,7 +127,7 @@
 				                <option value="56">56</option>
 				                <option value="57">57</option>
 				                <option value="58">58</option>
-				                <option value="59">59</option>					   
+				                <option value="59">59</option>
 							</select>
 					  	</div>
 					  	<div class="form-group align-time col-md-3">
@@ -153,7 +153,7 @@
 							    <option value="17">17</option>
 							    <option value="18">18</option>
 							    <option value="19">19</option>
-							    <option value="20">20</option>				   
+							    <option value="20">20</option>
 							    <option value="21">21</option>
 							    <option value="22">22</option>
 							    <option value="23">23</option>
@@ -192,21 +192,21 @@
 				                <option value="56">56</option>
 				                <option value="57">57</option>
 				                <option value="58">58</option>
-				                <option value="59">59</option>					   
+				                <option value="59">59</option>
 							</select>
 						</div>
 					</div>
-					<div class="align_height align_margin">					
+					<div class="align_height align_margin">
 						<label>Venue</label><br>
 						<textarea class="area_width" name="schedule_venue" data-validation-error-msg="please Enter the name of the Venue" data-validation="required"></textarea>
 					</div>
-					<div class="col-md-9 schedule_btn">	
-						<input type="reset" class="btn btn-primary align_right clear" value="Clear">	
-						<input type="submit" class="btn btn-primary align_right test-submit clear add_createschedule_act" value="Submit">									
-					</div>			
+					<div class="col-md-9 schedule_btn">
+						<input type="reset" class="btn btn-primary align_right clear" value="Clear">
+						<input type="submit" class="btn btn-primary align_right test-submit clear add_createschedule_act" value="Submit">
+					</div>
 				</form>
-			</div>			
-			<div class="container table-position">           
+			</div>
+			<div class="container table-position">
 			  <table class="table createschedule_table">
 			    <thead>
 			      <tr class="row_color">
@@ -230,29 +230,29 @@
 					        <td class="t_createschedule_s_id"><?php echo $i; ?></td>
 					        <td class="t_createschedule_name"><?php echo $row['createschedule_name']; ?></td>
 					        <td class="t_testbattery_name"><?php echo $row['testbattery_name']; ?></td>
-					        <td class="t_createschedule_date"><?php echo $row['createschedule_date']; ?></td>
+					        <td class="t_createschedule_date"><?php echo date("d/m/Y", strtotime($row['createschedule_date'])); ?></td>
 					        <td class="t_createschedule_time"><?php echo $row['createschedule_time']; ?></td>
 					        <td class="t_createschedule_venue"><?php echo $row['createschedule_venue']; ?></td>
 					        <td>
 					        	<span class="edit_state" onclick="editfunction(<?php echo $row['createschedule_id'] ?>)">Edit</span>
 					        	<span class="delete_state" data-value="<?php echo $row['createschedule_id'] ?>">Delete</span>
-					        </td> 				    
-			        </tr>                         
-                 <?php $i++;} ?>	 			   
+					        </td>
+			        </tr>
+                 <?php $i++;} ?>
 		    </tbody>
 		  </table>
-		</div>			
+		</div>
 	</div>
-	<!-- <div class="container align_center">		          
+	<!-- <div class="container align_center">
 	  	<ul class="pagination">
-	  		<li><a href="#" class="align_left_icon"><i class="fa fa-angle-double-left"></i></a></li>    	
+	  		<li><a href="#" class="align_left_icon"><i class="fa fa-angle-double-left"></i></a></li>
 		    <li><a href="#">1</a></li>
 		    <li><a href="#">2</a></li>
 		    <li><a href="#">3</a></li>
 		    <li><a href="#">4</a></li>
 		    <li><a href="#">5</a></li>
 		    <li><a href="#" class="align_right_icon"><i class="fa fa-angle-double-right"></i></a></li>
-		</ul>		   
+		</ul>
 	</div> --><!-- end  container-->
 </div><!-- end  container-->
 
@@ -263,11 +263,11 @@
           		<div class="edit_title">
                 	<span class="del_txt">EDIT</span>
               	</div><!--edit_title-->
-          			<div class="container state-content col-md-12">	
-          			<div class="col-xs-12 col-md-12 align_margin">	
+          			<div class="container state-content col-md-12">
+          			<div class="col-xs-12 col-md-12 align_margin">
 	          			<form name="edit_createschedule_form" id="edit_create_schedule_form">
 	          			<input type="hidden" class="statesid" name="edit_schedule_id">
-	          				<div class="form-group">					
+	          				<div class="form-group">
 						<label>Enter the schedule Name</label><br>
 						<input type="text" class="adjust_width" name="edit_schedule_name" data-validation-error-msg="Please Enter the name of the Schedule" data-validation="required">
 					</div>
@@ -279,11 +279,11 @@
 	                        $query = $testbattery->testbatterySelect();
 	                        while ($row = mysql_fetch_array($query)) {
 	                            ?>
-	                            <option value="<?php echo $row['testbattery_id']; ?>"><?php echo $row['testbattery_name']; ?></option>	                                            
-	                      <?php } ?>							   
+	                            <option value="<?php echo $row['testbattery_id']; ?>"><?php echo $row['testbattery_name']; ?></option>
+	                      <?php } ?>
 					  </select>
 					</div>
-					
+
 					<div class="form-group align-day">
 					  <label for="date" class="fl">Select date</label><br>
 					  <input class="popup_date_pick" type="text">
@@ -291,22 +291,22 @@
 					  	<option value="">Date</option>
 					    <option value="1">1</option>
 					    <option value="2">2</option>
-					    <option value="3">3</option>					   
+					    <option value="3">3</option>
 					  </select>
 					  <select class="form-control classic create-date2 fl" id="month" name="edit_schedule_month" data-validation-error-msg="Please Select the Month" data-validation="required">
 					  	<option value="">Month</option>
 					    <option value="1">January</option>
 					    <option value="2">February</option>
-					    <option value="3">March</option>						   
+					    <option value="3">March</option>
 					  </select>
 					  <select class="form-control classic create-date3 fl" id="year" name="edit_schedule_year" data-validation-error-msg="Please Select the Year" data-validation="required">
 					  	<option value="">Years</option>
 					    <option value="1991">1991</option>
 					    <option value="1992">1992</option>
-					    <option value="1993">1993</option>							   
+					    <option value="1993">1993</option>
 					  </select> -->
 					</div>
-					
+
 					<div class="form-group col-md-12">
 					  <label for="date" class="fl">Select Time</label><br>
 					  	<div class="col-md-3">
@@ -315,7 +315,7 @@
 							    <option value="01">01</option>
 							    <option value="02">02</option>
 							    <option value="03">03</option>
-							    <option value="04">04</option>						   
+							    <option value="04">04</option>
 							    <option value="05">05</option>
 							    <option value="06">06</option>
 							    <option value="07">07</option>
@@ -323,11 +323,11 @@
 							    <option value="09">09</option>
 							    <option value="10">10</option>
 							    <option value="11">11</option>
-							    <option value="12">12</option>							 
+							    <option value="12">12</option>
 							    <option value="01">13</option>
 							    <option value="02">14</option>
 							    <option value="03">15</option>
-							    <option value="04">16</option>						   
+							    <option value="04">16</option>
 							    <option value="05">17</option>
 							    <option value="06">18</option>
 							    <option value="07">19</option>
@@ -335,7 +335,7 @@
 							    <option value="09">21</option>
 							    <option value="10">22</option>
 							    <option value="11">23</option>
-							    <option value="12">24</option>			
+							    <option value="12">24</option>
 							</select>
 						</div>
 						<div class="col-md-3">
@@ -361,7 +361,7 @@
 							    <option value="17">17</option>
 							    <option value="18">18</option>
 							    <option value="19">19</option>
-							    <option value="20">20</option>				   
+							    <option value="20">20</option>
 							    <option value="21">21</option>
 							    <option value="22">22</option>
 							    <option value="23">23</option>
@@ -426,7 +426,7 @@
 						    <option value="17">17</option>
 						    <option value="18">18</option>
 						    <option value="19">19</option>
-						    <option value="20">20</option>				   
+						    <option value="20">20</option>
 						    <option value="21">21</option>
 						    <option value="22">22</option>
 						    <option value="23">23</option>
@@ -465,21 +465,21 @@
 			                <option value="56">56</option>
 			                <option value="57">57</option>
 			                <option value="58">58</option>
-			                <option value="59">59</option>				   
+			                <option value="59">59</option>
 					  	</select>
 				  	</div>
 				</div>
-			
-					<div class="align_height align_margin">					
+
+					<div class="align_height align_margin">
 						<label>Venue</label><br>
 						<textarea class="area_width_create" name="edit_schedule_venue" data-validation-error-msg="Please Enter the Venue" data-validation="required"></textarea>
 					</div>
-					<div class="col-md-10 schedule_btn">					
+					<div class="col-md-10 schedule_btn">
 						<!-- <button type="button" class="btn btn-primary align_right clear edit_createschedule_act">Submit</button>
 						<button type="button" class="btn btn-primary align_right clear">Clear</button> -->
 						<input type="reset" class="btn btn-primary align_right clear" value="Clear">
-						<input type="submit" class="btn btn-primary align_right test-submit clear edit_createschedule_act" value="Submit">						
-					</div>	
+						<input type="submit" class="btn btn-primary align_right test-submit clear edit_createschedule_act" value="Submit">
+					</div>
 	          	</form>
 	          		</div>
 					</div><!--state-content-->
