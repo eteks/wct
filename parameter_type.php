d7c11c62d6f5a21447585d53c386fbb56a7f0587
spmuthu21 <muthu@globalensolutions.com>
2016-04-15 11:15:39 +0530

    edit delete restriction added to parameter_type.php and parameter_unit.php

--- parameter_type.php
+++ parameter_type.php
@@ -43,10 +43,24 @@
 			        <td class="popup-edit">
 			        	<?php
 			        	if(strtolower(trim($row['parametertype_name'])) != 'time'){
-			        	?>
+				    	$que = mysql_query("select * from wc_test_attribute where test_parameter_type ='".$row['parametertype_name']."'");
+						$que1 = mysql_query("select * from wc_result where resultparameter_name ='".$row['parametertype_name']."'");
+						if(!mysql_num_rows($que) || !mysql_num_rows($que1)){
+	    				?>
+			        
 			        	<span class="edit_state" onclick="editfunction(<?php echo $row['parametertype_id'] ?>,this)"><i class="fa fa-pencil-square-o"></i></span>
 		        		<span class="delete_state" data-value="<?php echo $row['parametertype_id'] ?>"><i class="fa fa-trash-o"></i></span>
 						<?php
+						}else{
+						?>
+						<span class="restrict">
+			    			<i class="fa fa-pencil-square-o">
+				    			<div class="restrict_tooltip">Mapping has been already done.Edit or Delete not possible.</div>
+							</i>
+						</span>
+	    				<span class="restrict_del"><i class="fa fa-trash-o"><div class="restrict_tooltip">Mapping has been already done.Edit or Delete not possible.</div></i></span>
+						<?php
+						}
 						}
 						?>
 						<div class="state_div edit_parametertype_div popup_hidden">
